8ad422f98c52087ac092a1cf7c0aace01c7c6d7f
Vladyslav Strinada <v.strinada@bergermann.pl>
2017-08-23 08:50:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vladyslavstrinada/albumslist/AlertDialogManager.java
@@ -0,0 +1,39 @@
+package com.example.vladyslavstrinada.albumslist;
+
+/**
+ * Created by VladyslavStrinada on 22.08.2017.
+ */
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class AlertDialogManager {
+    /**
+     * Function to display simple Alert Dialog
+     * @param context - application context
+     * @param title - alert dialog title
+     * @param message - alert message
+     *
+     * */
+    public void showAlertDialog(Context context, String title, String message,
+                                Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        // Setting Dialog Title
+        alertDialog.setTitle(title);
+
+        // Setting Dialog Message
+        alertDialog.setMessage(message);
+
+
+        // Setting OK Button
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+}
